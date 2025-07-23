3f85aebf2688bffda89138be82ba1de3fe3b3728
David Montoya <davidmontoyago@gmail.com>
2025-07-23 17:58:08 -0500

    configure secret deletion protection

--- pkg/fullstack/gcp/secretmanager.go
+++ pkg/fullstack/gcp/secretmanager.go
@@ -6,23 +6,22 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func (f *FullStack) newEnvConfigSecret(ctx *pulumi.Context, serviceName, region, project string, serviceAccount *serviceaccount.Account) (*secretmanager.Secret, error) {
+func (f *FullStack) newEnvConfigSecret(ctx *pulumi.Context, serviceName string, serviceAccount *serviceaccount.Account, deletionProtection bool, labels pulumi.StringMap) (*secretmanager.Secret, error) {
 	secretID := f.newResourceName(serviceName, "config-secret", 100)
 
 	configSecret, err := secretmanager.NewSecret(ctx, secretID, &secretmanager.SecretArgs{
-		Labels: pulumi.StringMap{
-			"frontend": pulumi.String("true"),
-		},
+		Labels: labels,
 		Replication: &secretmanager.SecretReplicationArgs{
 			UserManaged: &secretmanager.SecretReplicationUserManagedArgs{
 				Replicas: secretmanager.SecretReplicationUserManagedReplicaArray{
 					&secretmanager.SecretReplicationUserManagedReplicaArgs{
-						Location: pulumi.String(region),
+						Location: pulumi.String(f.Region),
 					},
 				},
 			},
 		},
-		SecretId: pulumi.String(secretID),
+		SecretId:           pulumi.String(secretID),
+		DeletionProtection: pulumi.Bool(deletionProtection),
 	})
 	if err != nil {
 		return nil, err
@@ -31,7 +30,7 @@ func (f *FullStack) newEnvConfigSecret(ctx *pulumi.Context, serviceName, region,
 	// allow the instance GSA to access the secret
 	secretAccessorName := f.newResourceName(serviceName, "secret-accessor", 100)
 	_, err = secretmanager.NewSecretIamMember(ctx, secretAccessorName, &secretmanager.SecretIamMemberArgs{
-		Project:  pulumi.String(project),
+		Project:  pulumi.String(f.Project),
 		SecretId: configSecret.SecretId,
 		Role:     pulumi.String("roles/secretmanager.secretAccessor"),
 		Member:   pulumi.Sprintf("serviceAccount:%s", serviceAccount.Email),
